7695c19a9858309f9211e19145a83ef4d8c727d4
r47 <roman.hotin.rh@gmail.com>
2021-06-11 16:25:22 -0400

    commit 2

--- app/src/main/java/com/rhappdeveloper/inventoryapp/api/ApiInterface.java
+++ app/src/main/java/com/rhappdeveloper/inventoryapp/api/ApiInterface.java
@@ -9,6 +9,8 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ApiInterface {
     @GET
@@ -17,4 +19,12 @@ public interface ApiInterface {
     @POST
     Call<PartObject> createPart(@Body PartObject partObject);
 
+    @PUT("{id}")
+    Call<PartObject> UpdatePart(@Path ("id") int id,@Body PartObject partObject);
+
+    @PUT("{id}/receive")
+    Call<PartObject> ReceivePart(@Path ("id") int id, @Body PartObject partObject);
+
+    @PUT("{id}/consume")
+    Call<PartObject> ConsumePart(@Path ("id") int id, @Body PartObject partObject);
 }
